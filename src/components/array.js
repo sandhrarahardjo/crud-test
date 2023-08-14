9eb8f4e56b9a20236619a72f7a63d447a38ede04
Sandhra Rahardjo <ailhardaisy@gmail.com>
2023-08-14 10:11:03 +0700

    test

---
+++ src/components/array.js
@@ -0,0 +1,19 @@
+const array = [
+    {
+        id:'1', 
+        Name:'A',
+        Age:'23'
+    },
+    {
+        id:'2', 
+        Name:'B',
+        Age:'22'
+    },
+    {
+        id:'3',
+        Name:'C',
+        Age:'23'
+    }
+]
+  
+export default array
